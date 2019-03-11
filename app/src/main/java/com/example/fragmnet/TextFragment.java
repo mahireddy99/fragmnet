0c351ae4676ede4779ce8d74cd70b14f1261f798
mahi <mahi@132.com>
2019-03-11 12:16:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragmnet/TextFragment.java
@@ -0,0 +1,41 @@
+package com.example.fragmnet;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class TextFragment extends Fragment {
+
+    private TextView textView = null;
+
+    /* Used to change textview text size. */
+    public void changeTextSize(float textSize)
+    {
+        if(textView!=null)
+        {
+            textView.setTextSize(textSize);
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate TextFragment layout.
+        View fragmentView = inflater.inflate(R.layout.text_fragment, container, false);
+
+        // Get the textview object.
+        textView = fragmentView.findViewById(R.id.text_view);
+
+        return fragmentView;
+    }
+}
+
